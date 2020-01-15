e6926bc67f9abb32206d4cce7c7bffffa4fa9d9a
jhuizy <jhuizenga99@gmail.com>
2020-01-16 09:32:14 +1100

    typography working

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,50 @@
-import { Link } from "gatsby"
+import { Link, useStaticQuery, graphql } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+import styled from "styled-components"
+import tw from "tailwind.macro"
+
+const Nav = styled.div`
+  ${tw`bg-white-400 px-2 mx-auto flex justify-between items-center`}
+`
+
+const NavContainer = styled.header`
+  ${tw`flex-row items-center`}  
+`
+
+const NavHeading = styled.h1`
+  ${tw`text-black text-bold sm:text-3xl text-2xl font-display p-2`}
+`
+
+const Menu = styled.div`
+  ${tw`bg-blue-500 py-2 px-2 flex-row justify-around items-center`}
+`
+
+const MenuItem = styled.a`
+  ${tw`block text-black hover:bg-blue-800 hover:text-blue-200 mx-2 py-1 px-2 rounded`}
+`
+
+const NavOptions = styled.div`
+ ${tw``};
+`
+
+export default ({ siteTitle }) => (
+  <NavContainer>
+    <Nav>
+      <NavHeading>
+        {siteTitle}
+      </NavHeading>
+
+      <NavOptions>
+
+      </NavOptions>
+    </Nav>
+{/* 
+    <Menu>
+      <MenuItem href="#"><bold>Home</bold></MenuItem>
+      <MenuItem href="#"><bold>Posts</bold></MenuItem>
+      <MenuItem href="#"><bold>Contact</bold></MenuItem>
+    </Menu> */}
+  </NavContainer>
 )
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
-
-export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+import { FluidImage } from "../components/image"
 import SEO from "../components/seo"
 
 import styled from "styled-components"
@@ -15,7 +15,7 @@ const IndexPage = () => (
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+      <Image filepath="gatsby-astronaut.jpg" />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
