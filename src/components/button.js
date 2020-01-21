6133b665a354b9a8252fb04fa4447c83c821483c
jhuizy <jhuizenga99@gmail.com>
2020-01-21 20:01:59 +1100

    contact form implementation

--- src/components/button.js
+++ src/components/button.js
@@ -3,9 +3,10 @@ import React from 'react';
 import styled from 'styled-components'
 import tw from 'tailwind.macro'
 
-const Button = styled.div`
- ${tw`cursor-pointer hover:bg-teal-600 bg-teal-400 py-1 px-6 rounded-sm text-white font-bold font-sans text-xs hover:scale-110 transition-transform transition-bg transition-500 transition-ease`};
+const Button = styled.button`
+ ${tw`hover:bg-teal-600 bg-teal-400 py-1 px-6 rounded-sm text-white font-bold font-sans text-xs hover:scale-110 transition-transform transition-bg transition-500 transition-ease`};
  transition: transform 0.2s ease;
+ ${props => props.disabled ? "cursor: not-allowed;" : "cursor: pointer;"}
 `
 
 export default Button;
--- src/components/contact.js
+++ src/components/contact.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components'
 import tw from 'tailwind.macro'
 
@@ -13,32 +13,124 @@ const Label = styled.label`
 `
 
 const Input = styled.input`
- ${tw`m-2 border rounded px-2 md:w-full`};
+ ${tw`appearance-none m-2 border rounded px-2 md:w-full`};
 `
 const TextArea = styled.textarea`
  ${tw`m-2 border rounded px-2 md:w-full`};
 `
 
-export default () =>
-  <div class="w-full flex flex-col justify-around items-center">
-    <h1 className="text-lg lg:text-3xl">Contact Us</h1>
-    <form className="p-4 w-full md:w-8/12 lg:w-1/2 flex flex-col justify-around items-start">
-      <FieldSet>
-        <Label for="name">Name:</Label>
-        <Input className="m-2 border rounded px-2" id="name" type="text" placeholder="John Doe" />
-      </FieldSet>
-      <FieldSet>
-        <Label for="email">Email:</Label>
-        <Input className="m-2 border rounded px-2" id="email" type="email" placeholder="me@example.com" />
-      </FieldSet>
-      <FieldSet>
-        <Label for="message">Message:</Label>
-        <TextArea id="message" type="text" placeholder="Enter a message..." />
-      </FieldSet>
-      <FieldSet>
-        <Button className="w-20" type="submit">Send</Button>
-        <div className="flex-grow"/>
-      </FieldSet>
+const STATE_INIT = "init"
+const STATE_NORMAL = "normal"
+const STATE_SENDING = "sending"
+const STATE_SENT = "sent"
 
-    </form>
-  </div>
+const calculateBorderColor = (sendState, isValid) => {
+  switch(sendState) {
+    case STATE_INIT:
+      return "border-black-500"
+    default:
+      return isValid ? "border-green-500" : "border-red-500"
+  }
+}
+
+const validateName = s => s && s.length > 3
+const validateEmail = s => s && s.length > 3 && s.indexOf("@") !== -1 && s.indexOf(".") !== -1
+const validateMessage = s => s && s.length > 3
+
+export default () => {
+
+
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [message, setMessage] = useState("")
+  const [sendState, setSendState] = useState(STATE_INIT)
+
+  const onUpdate = (key, value) => {
+    switch(key) {
+      case "name": 
+        setName(value)
+        break
+      case "email":
+        setEmail(value)
+        break
+      case "message":
+        setMessage(value)
+        break  
+    }
+
+    setSendState(STATE_NORMAL)
+  }
+
+  const allValid = () =>
+    validateName(name) && 
+    validateEmail(email) && 
+    validateMessage(message)
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    if (allValid()) {
+      setSendState(STATE_SENDING)
+      setTimeout(() => {
+        setSendState(STATE_SENT)
+      }, 1000)
+    }
+  }
+
+  return (
+    <div class="w-full flex flex-col justify-around items-center">
+      <h1 className="text-lg lg:text-3xl">Contact Us</h1>
+      <form 
+        className="p-4 w-full md:w-8/12 lg:w-1/2 flex flex-col justify-around items-start"
+        >
+        <FieldSet>
+          <Label for="name">Name:</Label>
+          <Input
+            id="name"
+            className={`m-2 border rounded px-2 ${calculateBorderColor(sendState, validateName(name))}`}
+            type="text"
+            placeholder="John Doe"
+            value={name}
+            disabled={sendState == STATE_SENT}
+            onChange={e => onUpdate("name", e.target.value)}
+          />
+        </FieldSet>
+        <FieldSet>
+          <Label for="email">Email:</Label>
+          <Input
+            id="email"
+            className={`m-2 border rounded px-2 ${calculateBorderColor(sendState, validateEmail(email))}`}
+            type="email"
+            placeholder="me@example.com"
+            disabled={sendState == STATE_SENT}
+            value={email}
+            onChange={e => onUpdate("email", e.target.value)}
+          />
+        </FieldSet>
+        <FieldSet>
+          <Label for="message">Message:</Label>
+          <TextArea
+            className={`${calculateBorderColor(sendState, validateMessage(message))}`}
+            id="message"
+            type="text"
+            placeholder="Enter a message..."
+            disabled={sendState == STATE_SENT}
+            value={message}
+            onChange={e => onUpdate("message", e.target.value)}
+          />
+        </FieldSet>
+        <FieldSet>
+          <Button 
+            className="m-4 text-center" 
+            type="submit"
+            disabled={sendState == STATE_SENT || !allValid()}
+            onClick={onSubmit}
+            >
+            { sendState == STATE_SENDING ? "Sending..." : sendState == STATE_SENT ? "Sent" : "Send" }
+          </Button>
+          <div className="flex-grow" />
+        </FieldSet>
+
+      </form>
+    </div>
+  )
+}
