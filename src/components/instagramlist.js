8dca107f54a2bdba1e74a37254468c43eea97d00
jhuizy <jhuizenga99@gmail.com>
2020-01-22 20:09:46 +1100

    fix instagram list

--- src/components/instagramlist.js
+++ src/components/instagramlist.js
@@ -6,8 +6,8 @@ const Post = ({ image, likes, comments, id }) =>
     <a target="_blank" href={`https://instagram.com/p/${id}`} className="block group overflow-hidden h-full relative z-0 w-1/2 lg:w-1/3 xl:w-1/4">
       <div className="m-4 overflow-hidden relative">
         <Img fluid={image} />
-        <div className="absolute bg-black opacity-50 inset-0 z-0 w-full h-full group-hover:opacity-0 transition-all transition-200 transition-ease" />
-        <div className="absolute text-white inset-0 z-10 w-full h-full flex flex-col justify-center items-center group-hover:opacity-0 transition-all transition-200 transition-ease">
+        <div className="absolute bg-black opacity-0 inset-0 z-0 w-full h-full group-hover:opacity-25 transition-all transition-200 transition-ease" />
+        <div className="absolute opacity-0 text-white inset-0 z-10 w-full h-full flex flex-col justify-center items-center group-hover:opacity-100 transition-all transition-200 transition-ease">
           <div className="flex flex-row items-center justify-center">
             <svg className="mx-1 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path class="heroicon-ui" d="M12.76 3.76a6 6 0 0 1 8.48 8.48l-8.53 8.54a1 1 0 0 1-1.42 0l-8.53-8.54a6 6 0 0 1 8.48-8.48l.76.75.76-.75zm7.07 7.07a4 4 0 1 0-5.66-5.66l-1.46 1.47a1 1 0 0 1-1.42 0L9.83 5.17a4 4 0 1 0-5.66 5.66L12 18.66l7.83-7.83z" /></svg>
             <text className="mx-1 text-lg text-bold">{likes || " 0"}</text>
