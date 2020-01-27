f84e22c9fffb7d8105cc615ba7ba382d21639c81
jhuizy <jhuizenga99@gmail.com>
2020-01-28 08:47:54 +1100

    add google analytics v2

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,6 +68,17 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-gtag`,
+      options: {
+        // your google analytics tracking id
+        trackingId: `UA-156928303-1`,
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // enable ip anonymization
+        anonymize: true,
+      },
+    },
     `gatsby-plugin-netlify`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
