6f2347599fa3fc851c416678c894958fb8ca58db
jhuizy <jhuizenga99@gmail.com>
2020-02-05 11:34:12 +1100

    use new headless wp installation

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,11 @@
+require("dotenv").config({
+  path: `.env`,
+})
+
+// ignore self signed cert error for connecting to wordpress
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
+
+console.log(`process.env   = ${JSON.stringify(process.env, null, 2)}`);
 module.exports = {
   siteMetadata: {
     title: `Two Travelling Aussies`,
@@ -51,17 +59,20 @@ module.exports = {
          * The base URL of the WordPress site without the trailingslash and the protocol. This is required.
          * Example : 'demo.wp-api.org' or 'www.example-site.com'
          */
-        baseUrl: `twotravellingaussies.wordpress.com`,
+        baseUrl: `travelblog.jhuizy.com`,
+        verbose: true,
         // The protocol. This can be http or https.
         protocol: `https`,
         // Indicates whether the site is hosted on wordpress.com.
         // If false, then the assumption is made that the site is self hosted.
         // If true, then the plugin will source its content on wordpress.com using the JSON REST API V2.
         // If your site is hosted on wordpress.org, then set this to false.
-        hostingWPCOM: true,
+        hostingWPCOM: false,
         // If useACF is true, then the source plugin will try to import the WordPress ACF Plugin contents.
         // This feature is untested for sites hosted on WordPress.com
         useACF: false,
+        jwt_user: process.env.JWT_USER,
+        jwt_pass: process.env.JWT_PASSWORD,
         plugins: [
           {
             resolve: `gatsby-wordpress-inline-images`,
